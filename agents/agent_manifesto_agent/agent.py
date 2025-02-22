c893afe64f5f70c9665f98853eb53b5b1a68cfd5
Leonard Tan <lentan1029@gmail.com>
2025-02-22 13:55:41 -0700

    rename agent gen, only gen manifesto, no writes

--- agents/agent_manifesto_agent/agent.py
+++ agents/agent_manifesto_agent/agent.py
@@ -1,8 +1,9 @@
 from lib.base import Agent
 from lib.tools.read_base_agent_implementation import read_base_agent_implementation
-from lib.tools.write_file_in_agents_folder import write_file_in_agents_folder
+from lib.tools.read_readme import read_readme
 from lib.tools.read_file_in_agents_folder import read_file_in_agents_folder
 from lib.tools.list_files_in_agents_folder import list_files_in_agents_folder
+from lib.tools.list_tools import list_tools
 
 def create_agent(
     manifesto: str,
@@ -13,9 +14,10 @@ def create_agent(
         memory=memory,
         tools={
             'READ_BASE_AGENT_IMPLEMENTATION': read_base_agent_implementation,
-            'WRITE_FILE_IN_AGENTS_FOLDER': write_file_in_agents_folder,
             'READ_FILE_IN_AGENTS_FOLDER': read_file_in_agents_folder,
-            'LIST_FILES_IN_AGENTS_FOLDER': list_files_in_agents_folder
+            'LIST_FILES_IN_AGENTS_FOLDER': list_files_in_agents_folder,
+            'READ_README': read_readme,
+            'LIST_TOOLS': list_tools
         },
-        name="AgentGenerationAgent",
+        name="AgentManifestoAgent",
     )

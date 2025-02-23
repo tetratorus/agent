983076cffee0db4ed2a130ce50a476dd2bf14619
Leonard Tan <lentan1029@gmail.com>
2025-02-23 16:57:53 -0700

    add rps agent

---
+++ agents/scissors_paper_stone_agent/agent.py
@@ -0,0 +1,12 @@
+from lib.base import Agent
+
+def create_agent(
+    manifesto: str,
+    memory: str,
+) -> Agent:
+    return Agent(
+        manifesto=manifesto,
+        memory=memory,
+        tools={},
+        name="ScissorsPaperStoneAgent",
+    )
